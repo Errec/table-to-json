9c694a2d646bc383c6ec8b6d7ab19e188cf02d5b
Raniro Coelho <errecme@gmail.com>
2017-06-14 15:59:49 -0300

    Add _writeJson function

--- table-to-json.js
+++ table-to-json.js
@@ -6,6 +6,8 @@ var tableToJson = function(inputTable, columnKey, columnValue) {
   var dataTable = _splitData(data).filter(function(n){return n[0] !== '';});
   var tableObj = _makeTableObj(dataTable, columnKey, columnValue);
 
+  _writeJson(tableObj);
+
   function _splitData(data) {
     var dataText = data.toString().split(/\r?\n/);
     return dataText.map(function(line) {
@@ -14,12 +16,17 @@ var tableToJson = function(inputTable, columnKey, columnValue) {
   }
 
   function _makeTableObj(matrix, columnKey, columnValue) {
-    return matrix.reduce(function (o, currentArray) {
+    return matrix.reduce(function (obj, currentArray) {
         var key = currentArray[columnKey], value = currentArray[columnValue];
-        o[key] = value;
-          return o;
+        obj[key] = value;
+        return obj;
     }, {});
+  }
+
+  function _writeJson(obj) {
+    var json = JSON.stringify(obj);
+    fs.writeFile('./temp/output.json', json, 'utf8');
   }
 };
 
-tableToJson(meso);
+tableToJson(meso, 2, 0);
