9369d9b55e841834134a8428929088d5cde57834
Raniro Coelho <errecme@gmail.com>
2017-06-14 20:42:21 -0300

    Reformat table-to-json code

--- table-to-json.js
+++ table-to-json.js
@@ -1,29 +1,30 @@
 var meso = './src/meso_micro_mun.txt';
 
 var tableToJson = function(inputTable, columnKey, columnValue) {
-  var fs = require('fs');
+  var fs   = require('fs');
   var data = fs.readFileSync(inputTable, "latin1");
-  var dataTable = _splitData(data).filter(function(n){return n[0] !== '';});
-  var tableObj = _makeTableObj(dataTable, columnKey, columnValue);
 
-  _writeJson(tableObj);
+  var dataTable = _splitData(data);
+  var tableObj  = _makeTableObj(dataTable, columnKey, columnValue);
+  _writeJsonFile(tableObj);
 
   function _splitData(data) {
-    var dataText = data.toString().split(/\r?\n/);
-    return dataText.map(function(line) {
-      return line.split("\t");
-    });
+    return data
+          .toString()
+          .split(/\r?\n/)
+          .map(function(line) { return line.split("\t"); })
+          .filter(function(line){ return line[0] !== ''; });
   }
 
   function _makeTableObj(matrix, columnKey, columnValue) {
     return matrix.reduce(function (obj, currentArray) {
-        var key = currentArray[columnKey], value = currentArray[columnValue];
-        obj[key] = value;
-        return obj;
+      var key = currentArray[columnKey], value = currentArray[columnValue];
+      obj[key] = value;
+      return obj;
     }, {});
   }
 
-  function _writeJson(obj) {
+  function _writeJsonFile(obj) {
     var json = JSON.stringify(obj);
     fs.writeFile('./temp/output.json', json, 'utf8');
   }
